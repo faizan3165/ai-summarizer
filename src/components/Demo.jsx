c96e20594e2e99a628a908a126ab8344e6905cb2
faizan <faizanmehmood3165@gmail.com>
2023-05-01 18:34:08 +0500

    demo section

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const demo = () => {
+import { copy, linkIcon, loader, tick } from "../assets";
+
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    alert("Submitted");
+  };
+
   return (
-    <div>demo</div>
-  )
-}
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          onSubmit={handleSubmit}
+          className="relative flex justify-center items-center"
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
 
-export default demo
+          <input
+            type="url"
+            placeholder="Paste or enter a url"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            <p>↵</p>
+          </button>
+        </form>
+
+        {/* Browse History */}
+      </div>
+
+      {/* Display Results */}
+    </section>
+  );
+};
+
+export default Demo;
